84f0c6c50234d1463120af1076d95bda4a72038a
PedroBurgos4 <pedroburgos4@gmail.com>
2025-08-13 20:04:18 -0600

    nueva rama creada

---
+++ src/com/prueba/git/CountToTen.java
@@ -0,0 +1,9 @@
+package com.prueba.git;
+
+public class CountToTen {
+    public void countToTen() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
