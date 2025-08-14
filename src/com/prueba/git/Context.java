63cc5898a562ef6ab38b00732dc4cadda7f35e72
PedroBurgos4 <pedroburgos4@gmail.com>
2025-08-13 20:13:26 -0600

    rama principal y secundaria

--- src/com/prueba/git/Context.java
+++ src/com/prueba/git/Context.java
@@ -3,8 +3,7 @@ package com.prueba.git;
 public class Context {
     public static void main(String[] args) {
         System.out.println("Cuenta hasta diez");
-       for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
+        CountToTen countToTen = new CountToTen();
+        countToTen.countToTen();
     }
 }
