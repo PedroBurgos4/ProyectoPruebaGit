a2d98e88b8688bae78d272cc24da669844b2c8f4
PedroBurgos4 <pedroburgos4@gmail.com>
2025-08-13 20:00:08 -0600

    Primer commit

---
+++ src/com/prueba/git/Context.java
@@ -0,0 +1,10 @@
+package com.prueba.git;
+
+public class Context {
+    public static void main(String[] args) {
+        System.out.println("Cuenta hasta diez");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
